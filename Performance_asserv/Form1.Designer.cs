e7bc746dd5d24a08d7764f61f3ce5f4f3953249b
Jeremy Joly <jeyjoly@gmail.com>
2019-09-24 18:21:01 -0400

    First commit Performances asservissement

---
+++ Performance_asserv/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Quadratic
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Quadratic));
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonCalculate = new System.Windows.Forms.Button();
+            this.textBoxA = new System.Windows.Forms.TextBox();
+            this.textBoxB = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.XiBox = new System.Windows.Forms.TextBox();
+            this.WnBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.EC_Box = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(201, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(274, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Criteres de performance";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // buttonCalculate
+            // 
+            this.buttonCalculate.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.buttonCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCalculate.Location = new System.Drawing.Point(265, 256);
+            this.buttonCalculate.Name = "buttonCalculate";
+            this.buttonCalculate.Size = new System.Drawing.Size(100, 35);
+            this.buttonCalculate.TabIndex = 1;
+            this.buttonCalculate.Text = "Calculate";
+            this.buttonCalculate.UseVisualStyleBackColor = false;
+            this.buttonCalculate.Click += new System.EventHandler(this.buttonCalculate_Click);
+            // 
+            // textBoxA
+            // 
+            this.textBoxA.Location = new System.Drawing.Point(86, 78);
+            this.textBoxA.Name = "textBoxA";
+            this.textBoxA.Size = new System.Drawing.Size(105, 22);
+            this.textBoxA.TabIndex = 2;
+            this.textBoxA.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBoxA.TextChanged += new System.EventHandler(this.textBoxA_TextChanged);
+            // 
+            // textBoxB
+            // 
+            this.textBoxB.Location = new System.Drawing.Point(426, 78);
+            this.textBoxB.Name = "textBoxB";
+            this.textBoxB.Size = new System.Drawing.Size(100, 22);
+            this.textBoxB.TabIndex = 3;
+            this.textBoxB.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBoxB.TextChanged += new System.EventHandler(this.textBoxB_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(115, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Tr(5%)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(466, 58);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "D";
+            // 
+            // XiBox
+            // 
+            this.XiBox.Location = new System.Drawing.Point(166, 153);
+            this.XiBox.Name = "XiBox";
+            this.XiBox.Size = new System.Drawing.Size(94, 22);
+            this.XiBox.TabIndex = 8;
+            this.XiBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // WnBox
+            // 
+            this.WnBox.Location = new System.Drawing.Point(364, 153);
+            this.WnBox.Name = "WnBox";
+            this.WnBox.Size = new System.Drawing.Size(95, 22);
+            this.WnBox.TabIndex = 9;
+            this.WnBox.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(203, 133);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(20, 17);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Xi";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(399, 133);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(29, 17);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Wn";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(197, 83);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 17);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Secondes";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(465, 158);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 17);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "rad/s";
+            // 
+            // EC_Box
+            // 
+            this.EC_Box.Location = new System.Drawing.Point(206, 217);
+            this.EC_Box.Name = "EC_Box";
+            this.EC_Box.Size = new System.Drawing.Size(209, 22);
+            this.EC_Box.TabIndex = 15;
+            this.EC_Box.TextChanged += new System.EventHandler(this.EQ_Box_TextChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(233, 197);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(163, 17);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Équation Caractéristique";
+            // 
+            // Quadratic
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(618, 312);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.EC_Box);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.WnBox);
+            this.Controls.Add(this.XiBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxB);
+            this.Controls.Add(this.textBoxA);
+            this.Controls.Add(this.buttonCalculate);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Quadratic";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Performance";
+            this.Load += new System.EventHandler(this.Quadratic_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonCalculate;
+        private System.Windows.Forms.TextBox textBoxA;
+        private System.Windows.Forms.TextBox textBoxB;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox XiBox;
+        private System.Windows.Forms.TextBox WnBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox EC_Box;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Performance_asserv/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Quadratic : Form
+    {
+        public Quadratic()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonCalculate_Click(object sender, EventArgs e)
+        {
+            double Tr = double.Parse(textBoxA.Text);
+            double D = double.Parse(textBoxB.Text);
+            if (D<=0)
+            {
+                MessageBox.Show("D doit être strictement positif");
+            }
+            else
+            {
+                double Xi = Math.Sqrt((Math.Pow(Math.Log(D),2))/((Math.Pow(Math.Log(D), 2))+Math.Pow(Math.PI,2)));
+                double Wn = 3/(Tr*Xi);
+                double b = 2 * Xi * Wn;
+                double c = Math.Pow(Wn, 2);
+                string XiText = Xi.ToString("0.0000");
+                string WnText = Wn.ToString("0.0000");
+                string bText = b.ToString("0.000");
+                string cText = c.ToString("0.000");
+                XiBox.Text = XiText;
+                WnBox.Text = WnText;
+                EC_Box.Text = "s^2 + "+bText+"s + "+cText;
+
+            }
+            
+            
+            
+        }
+
+        private void textBoxA_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxB_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxC_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Quadratic_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void EQ_Box_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
